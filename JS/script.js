e3bb6d485a84d4ff742b446e1304bfe01ba71dce
fokstreet <69371778+fokstreet@users.noreply.github.com>
2020-09-15 13:55:17 +0300

    DZ за 10.09.2020
    1) Скрипт умеет выбирать случайное слово из массива keywords и писать его в поисковике yandex.
    2) Скрипт кликает на поиск по случайно выбранному слову
    3) Скрипт находит исокмый сайт и кликает по его ссылке.

---
+++ JS/script.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Bot for Yandex
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://yandex.ru/*
+// @grant        none
+// ==/UserScript==
+
+let keywords =['Гобой', 'Как звучит флейта', 'Кларнет'];
+let keyword = keywords[getRandom(0, keywords.length)];
+let poisk = document.getElementById('text');
+if (poisk != undefined){
+    document.getElementsByName('text')[0].value = keyword;
+    document.getElementsByTagName("button")[0].click()
+}else{
+    let links = document.links;
+
+    for (let i=0; i<links.length; i++){
+        if(links[i].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") != -1){
+            console.log("Ссылка найдена" + links[i]);
+            links[i].click();
+            break;
+        }
+    }
+}
+function getRandom(min, max){
+    return Math.floor(Math.random()*(max-min)+min);
+}
