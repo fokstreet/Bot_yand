e7b48c7db041591a906da995c2e6e36166e61173
fokstreet <69371778+fokstreet@users.noreply.github.com>
2020-09-18 17:30:26 +0300

    Update script.js
    Не хочет записывать правильно куки

--- JS/script.js
+++ JS/script.js
@@ -6,17 +6,31 @@
 // @author       You
 // @match        https://yandex.ru/*
 // @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
+// @match        https://crushdrummers.ru/*
 // @grant        none
 // ==/UserScript==
+let sites = {
+    "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai":['Гобой', 'Как звучит флейта', 'Кларнет', 'Саксофон'],
+    "crushdrummers.ru":['Барабанное шоу','Заказать барабанное шоу','Шоу барабанщиков в Москве']
+};
+let site = Object.keys(sites)[getRandom(0, Object.keys(sites).length)];
 let yandexInput = document.getElementById('text');
-let keywords =['Гобой', 'Как звучит флейта', 'Кларнет', 'Саксофон', 'Труба', 'Гитара'];
+let keywords =sites[site];
 let keyword = keywords[getRandom(0, keywords.length)];
 let btnY = document.getElementsByTagName("button")[0];
+let i = 0;
 let links = document.links; // вся коллекция ссылок
 let next = document.getElementsByClassName('pager_item_kind_next');
-let i = 0;
 
-if (yandexInput != undefined){
+if (yandexInput != null){
+    document.cookie = "site="+site; // "site+"=site;
+}else if (location.hostname=="yandex.ru"){
+    site = getCookie("site");
+}else{
+    site=location.hostname;
+}
+
+if (yandexInput != null){
     let timerId = setInterval(()=>{
         yandexInput.value+=keyword[i]; //вбивает слово в поисковик. По одной букве с интервалом, заданным рандомно
         i++;
@@ -24,14 +38,14 @@ if (yandexInput != undefined){
             clearInterval(timerId);
             btnY.click();
         }
-    },1000*getRandom(1, 3));
-}else if(location.hostname == "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai"){
+    },2000);
+}else if(location.hostname == site){
     setInterval(()=>{
         let index = getRandom (0, links.length);
-        if (getRandom(0, 101)>=75){ //вероятность возврата на главную страницу яндекс
+        if (getRandom(0, 101)>=80){ //вероятность возврата на главную страницу яндекс
             location.href='https://yandex.ru/';
         }
-        if (links[index].href.indexOf ("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") !=-1){ // алгоритм для возврата на сайт, выбранный  для прогулки
+        else if (links[index].href.indexOf (site) !=-1){ // алгоритм для возврата на сайт, выбранный  для прогулки
             links.removeAttribute('target');
             links[index].click();
         }
@@ -39,11 +53,11 @@ if (yandexInput != undefined){
 }else{
     let nextYandexPage = true;
     for (let i=0; i<links.length; i++){
-        if(links[i].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") != -1){
+        if(links[i].href.indexOf(site) != -1){
             let link = links[i];
             nextYandexPage = false;
+            links.removeAttribute('target');
             setTimeout(()=>{link.click();}, getRandom(1000, 4000));
-            link.removeAttribute('target');
             break;
         }
     }
@@ -60,4 +74,10 @@ if (yandexInput != undefined){
 
 function getRandom(min, max){
     return Math.floor(Math.random()*(max-min)+min);
+}
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
 }
